322bb1114ff33a73f97c88e06817de3615caa9c8
hwellmann <hwellmann@localhost>
2010-09-10 15:44:35 +0200

    factored out GlassfishTestDriver for reuse in different test suites

--- jeeunit-example/jeeunit-example-test/src/test/java/com/googlecode/jeeunit/example/test/driver/EmbeddedGlassfishTest.java
+++ jeeunit-example/jeeunit-example-test/src/test/java/com/googlecode/jeeunit/example/test/driver/EmbeddedGlassfishTest.java
@@ -17,65 +17,32 @@
 
 package com.googlecode.jeeunit.example.test.driver;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.io.IOException;
 
-import org.glassfish.api.deployment.DeployCommandParameters;
-import org.glassfish.api.embedded.ContainerBuilder;
-import org.glassfish.api.embedded.EmbeddedDeployer;
-import org.glassfish.api.embedded.EmbeddedFileSystem;
 import org.glassfish.api.embedded.LifecycleException;
-import org.glassfish.api.embedded.Server;
 import org.junit.Test;
 
-import com.sun.jersey.api.client.Client;
+import com.googlecode.jeeunit.glassfish.GlassfishTestDriver;
 
 
-public class EmbeddedGlassfishTest {
+public class EmbeddedGlassfishTest extends GlassfishTestDriver {
 
 
-    @Test
-    public void runServer() throws IOException, LifecycleException, InterruptedException {
-        Server.Builder builder = new Server.Builder("IntegrationTest");
-        builder.verbose(false);
-        builder.logger(false);
-
-        // Define your JDBC resources and JNDI names in this config file
+    public EmbeddedGlassfishTest() {
         File domainConfig = new File("src/test/resources/domain.xml");
+        setConfiguration(domainConfig);
+        
+        setApplicationName("jeeunit.example");
+        setContextRoot("jeeunit");
 
-        // Build a file system for the embedded server
-        EmbeddedFileSystem.Builder efsb = new EmbeddedFileSystem.Builder();
-        efsb.configurationFile(domainConfig);
-        String tmpDir = System.getProperty("java.io.tmpdir");
-        efsb.instanceRoot(new File(tmpDir, "gfembed"));
-        EmbeddedFileSystem efs = efsb.build();
-        builder.embeddedFileSystem(efs);
-
-        // Build the server, including all containers (web, jpa, ejb, ...)
-        Server server = builder.build();
-        server.addContainer(ContainerBuilder.Type.all);
-        server.start();
-
-        // Deploy your test app. Make sure to use the correct path
         File war = new File("target/jeeunit-example-test.war");
-        EmbeddedDeployer deployer = server.getDeployer();
-        DeployCommandParameters params = new DeployCommandParameters();
-        params.contextroot = "itest";
-        deployer.deploy(war, params);
-
-        // Do a GET request on the URL of your test servlet. Make sure
-        // the URL matches your server, context root, and the URL pattern
-        // used by your servlet
-        Client client = Client.create();
-        String result = client.resource("http://localhost:8080/itest/test").get(String.class);
+        setWarFile(war);
+    }
+    
+    @Test
+    public void runTests() throws IOException, LifecycleException, InterruptedException {
         
-        // The servlet simply returns plain text.
-        assertTrue(result.contains("All tests passed"));
-        
-        // Stop the server so that the test driver can terminate
-        deployer.undeployAll();
-        server.stop();
+        runServer();
     }
 }
