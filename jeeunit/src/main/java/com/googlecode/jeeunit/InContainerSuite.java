b69bc19444c9d6a6610718341d9d3a39781e8c8f
hwellmann <hwellmann@localhost>
2010-09-10 17:21:35 +0200

    filter test classes by pattern specified as system property jeeunit.testClassPattern

--- jeeunit/src/main/java/com/googlecode/jeeunit/InContainerSuite.java
+++ jeeunit/src/main/java/com/googlecode/jeeunit/InContainerSuite.java
@@ -17,25 +17,56 @@
 
 package com.googlecode.jeeunit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.runner.Runner;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 
+/**
+ * Runs all test classes specified by a {@code SuiteClasses} annotation in an embedded server.
+ * The list of suite classes can be filtered by a regular expression specified in the system property
+ * {@code jeeunit.testClassPattern}. If this property is set, only the classes with fully
+ * qualified class name matching the pattern will be executed.
+ *
+ */
 public class InContainerSuite extends Suite {
 
-    public InContainerSuite(Class<?> klass)
-            throws InitializationError {
-        super(klass, new InContainerRunnerBuilder());
+    public static final String KEY_TEST_CLASS_PATTERN = "jeeunit.testClassPattern";
+
+    public InContainerSuite(Class<?> klass) throws InitializationError {
+        super(new InContainerRunnerBuilder(), klass, getSuiteClasses(klass));
     }
 
-    static class InContainerRunnerBuilder extends RunnerBuilder
-    {
+    private static Class<?>[] getSuiteClasses(Class<?> klass) throws InitializationError {
+        SuiteClasses annotation = klass.getAnnotation(SuiteClasses.class);
+        if (annotation == null) {
+            String msg = String.format("class '%s' must have a SuiteClasses annotation", klass
+                    .getName());
+            throw new InitializationError(msg);
+        }
+
+        String pattern = System.getProperty(KEY_TEST_CLASS_PATTERN);
+        if (pattern == null)
+            return annotation.value();
+
+        List<Class<?>> classes = new ArrayList<Class<?>>();
+        for (Class<?> testClass : annotation.value()) {
+            if (testClass.getName().matches(pattern)) {
+                classes.add(testClass);
+            }
+        }
+        Class<?>[] result = new Class<?>[classes.size()];
+        return classes.toArray(result);
+    }
+
+    static class InContainerRunnerBuilder extends RunnerBuilder {
 
         @Override
         public Runner runnerForClass(Class<?> testClass) throws Throwable {
             return new CdiJUnitRunner(testClass);
-        }       
-    }   
+        }
+    }
 }
-
