6ffa833b4a96839df52aeed4c5d63249e212f379
hwellmann <hwellmann@localhost>
2010-11-28 13:23:34 +0100

    added comments

--- jeeunit-example/jeeunit-example-test-spring/src/test/java/com/googlecode/jeeunit/example/test/spring/SpringTestConfig.java
+++ jeeunit-example/jeeunit-example-test-spring/src/test/java/com/googlecode/jeeunit/example/test/spring/SpringTestConfig.java
@@ -12,6 +12,22 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import com.googlecode.jeeunit.example.service.LibraryService;
 
+/**
+ * This class provides (almost all of) the Spring application context required for testing
+ * our example in Spring.
+ * <p>
+ * Since Spring 3.0.x still has a small number of configuration settings which are supported in
+ * XML syntax only, we take an XML-centric approach, where the application is bootstrapped via a
+ * traditional XML context which contains the XML-only settings and delegates all the rest to
+ * this class.
+ * <p>
+ * The <code>@Configuration</code> annotation instructs Spring to scan this class for bean
+ * provider methods with a <code>@Bean</code> annotation. Each such method yields a singleton
+ * Spring bean with its type and name defined by the method return type and name, respectively.
+ * 
+ * @author hwellmann
+ *
+ */
 @Configuration
 public class SpringTestConfig {
 
@@ -22,19 +38,31 @@ public class SpringTestConfig {
 
     @Bean
     public DataSource dataSource() {
-        SimpleDriverDataSource bean = new SimpleDriverDataSource(new org.apache.derby.jdbc.ClientDriver(),
+        return new SimpleDriverDataSource(new org.apache.derby.jdbc.ClientDriver(),
                 "jdbc:derby://localhost/sun-appserv-samples;create=true");
-
-        return bean;
     }
 
     @Bean
     public PlatformTransactionManager transactionManager() {
-      JpaTransactionManager tm = new JpaTransactionManager(entityManagerFactory().getObject());
-      return tm;
+      return new JpaTransactionManager(entityManagerFactory().getObject());
     }
     
 
+    /**
+     * This bean is not the EntityManagerFactory itself, but a Spring factory for JPA 
+     * EntityManagerFactories. The actual EntityManagerFactory can be obtained by invoking
+     * <code>getObject()</code>.
+     * <p>
+     * The Spring factory lets us override settings from the default <code>persistence.xml</code>.
+     * In particular, in the test environment we cannot access the data source via JNDI, and there
+     * is no JTA transaction manager. This is why we have to use an alternative 
+     * <code>test-persistence.xml</code>.
+     * <p>
+     * TODO: It would be nice to avoid the <code>test-persistence.xml</code> and to provide all the
+     * required overrides in this bean.
+     * 
+     * @return
+     */
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
