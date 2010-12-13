3c56e74d28f27dfe01c80bd6dc242d0487630907
hwellmann <hwellmann@localhost>
2010-12-13 23:44:01 +0100

    new module jeeunit-concurrent for concurrent and repeated tests, with or without paramters

---
+++ jeeunit-concurrent/src/main/java/com/googlecode/jeeunit/concurrent/Repeat.java
@@ -0,0 +1,12 @@
+package com.googlecode.jeeunit.concurrent;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface Repeat {
+    int times() default 1;
+}
