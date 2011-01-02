9cdd5831944a0a3c0b0626580671e570585552b8
hwellmann <hwellmann@localhost>
2011-01-02 17:05:57 +0100

    Run the Music example with a JUnit FitSuite. Test variant demonstrating errors vs. failures.

---
+++ jeeunit-fit-parent/jeeunit-fit-example/src/test/java/com/googlecode/jeeunit/fit/eg/music/suite/MusicFitSuite.java
@@ -0,0 +1,12 @@
+package com.googlecode.jeeunit.fit.eg.music.suite;
+
+import org.junit.runner.RunWith;
+
+import com.googlecode.jeeunit.fit.junit.FitConfiguration;
+import com.googlecode.jeeunit.fit.junit.FitSuite;
+
+@RunWith(FitSuite.class)
+@FitConfiguration(inputDir = "src/test/fit", outputDir = "target/fit")
+public class MusicFitSuite {
+
+}
