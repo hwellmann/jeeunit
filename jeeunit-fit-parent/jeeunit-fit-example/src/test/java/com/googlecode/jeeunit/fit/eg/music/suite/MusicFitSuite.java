4b219fa71d324094b4dcae46b8a74053d2307691
hwellmann <hwellmann@localhost>
2011-01-02 17:12:53 +0100

    added comment

--- jeeunit-fit-parent/jeeunit-fit-example/src/test/java/com/googlecode/jeeunit/fit/eg/music/suite/MusicFitSuite.java
+++ jeeunit-fit-parent/jeeunit-fit-example/src/test/java/com/googlecode/jeeunit/fit/eg/music/suite/MusicFitSuite.java
@@ -5,6 +5,11 @@ import org.junit.runner.RunWith;
 import com.googlecode.jeeunit.fit.junit.FitConfiguration;
 import com.googlecode.jeeunit.fit.junit.FitSuite;
 
+/**
+ * An example showing how to run a suite of FIT tests under JUnit.
+ * @author hwellmann
+ *
+ */
 @RunWith(FitSuite.class)
 @FitConfiguration(inputDir = "src/test/fit", outputDir = "target/fit")
 public class MusicFitSuite {
