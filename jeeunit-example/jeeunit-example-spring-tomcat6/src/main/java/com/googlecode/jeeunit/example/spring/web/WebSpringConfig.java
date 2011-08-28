0ae1dcf46fbc5543c74d0a7dc72a18f85ffa4906
hwellmann <hwellmann@localhost>
2011-08-28 20:49:47 +0200

    Tomcat 6 /Spring MVC example

---
+++ jeeunit-example/jeeunit-example-spring-tomcat6/src/main/java/com/googlecode/jeeunit/example/spring/web/WebSpringConfig.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2011 Harald Wellmann
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.googlecode.jeeunit.example.spring.web;
+
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.hibernate.ejb.HibernatePersistence;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.googlecode.jeeunit.example.service.LibraryService;
+import com.googlecode.jeeunit.example.spring.web.controller.LibraryController;
+
+@Configuration
+public class WebSpringConfig {
+
+    @Bean
+    public LibraryController libraryController() {
+        return new LibraryController();
+    }
+
+    @Bean
+    public LibraryService libraryService() {
+        return new LibraryService();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactory());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        JndiObjectFactoryBean factoryBean = new JndiObjectFactoryBean();
+        factoryBean.setJndiName("java:comp/env/jdbc/library");
+        try {
+            factoryBean.afterPropertiesSet();
+            return (DataSource) factoryBean.getObject();
+        }
+        catch (IllegalArgumentException exc) {
+            throw new RuntimeException(exc);
+        }
+        catch (NamingException exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setDataSource(dataSource());
+        bean.setPersistenceProvider(new HibernatePersistence());
+        bean.setPersistenceXmlLocation("classpath:META-INF/spring-persistence.xml");
+        bean.afterPropertiesSet();
+        return bean.getObject();
+    }
+    
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/jsp/");
+        bean.setSuffix(".jsp");
+        bean.setViewClass(JstlView.class);
+        return bean;
+    }
+}
