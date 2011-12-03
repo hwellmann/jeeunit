1a40e42c5c17fa01536060dc0de5734748263637
hwellmann <hwellmann@localhost>
2011-12-03 15:03:25 +0100

    example with Spring 3.1.RC2, Tomcat 7 and XML-free configuration

--- jeeunit-example/jeeunit-example-spring-tomcat7/src/main/java/com/googlecode/jeeunit/example/spring/web/LibraryWebApplicationInitializer.java
+++ jeeunit-example/jeeunit-example-spring-tomcat7/src/main/java/com/googlecode/jeeunit/example/spring/web/LibraryWebApplicationInitializer.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -13,10 +14,13 @@ public class LibraryWebApplicationInitializer implements WebApplicationInitializ
     @Override
     public void onStartup(ServletContext sc) throws ServletException {
 
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(WebSpringConfig.class);
+        sc.addListener(new ContextLoaderListener(rootContext));
+        
         // Create the dispatcher servlet's Spring application context
         AnnotationConfigWebApplicationContext dispatcherContext = new AnnotationConfigWebApplicationContext();
-        dispatcherContext.register(WebSpringConfig.class);
-
+        dispatcherContext.setParent(rootContext);
         // Register and map the dispatcher servlet
         ServletRegistration.Dynamic dispatcher = sc.addServlet("dispatcher",
                 new DispatcherServlet(dispatcherContext));
